427ab1c2160fdbe252621de9714d713ff605ca02
maks1u <smorldesign@gmail.com>
2024-04-18 14:41:32 +0200

    Added system

--- VideoEncoder/Form1.Designer.cs
+++ VideoEncoder/Form1.Designer.cs
@@ -28,13 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.has_image = new System.Windows.Forms.CheckBox();
+            this.video_load = new System.Windows.Forms.Button();
+            this.image_load = new System.Windows.Forms.Button();
+            this.view_mode = new System.Windows.Forms.GroupBox();
+            this.quad = new System.Windows.Forms.RadioButton();
+            this.dual = new System.Windows.Forms.RadioButton();
+            this.create_video = new System.Windows.Forms.Button();
+            this.name_video = new System.Windows.Forms.TextBox();
+            this.label_for_name = new System.Windows.Forms.Label();
+            this.four = new System.Windows.Forms.CheckBox();
+            this.third = new System.Windows.Forms.CheckBox();
+            this.second = new System.Windows.Forms.CheckBox();
+            this.first = new System.Windows.Forms.CheckBox();
+            this.view_mode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // has_image
+            // 
+            this.has_image.AutoSize = true;
+            this.has_image.Location = new System.Drawing.Point(239, 70);
+            this.has_image.Name = "has_image";
+            this.has_image.Size = new System.Drawing.Size(82, 17);
+            this.has_image.TabIndex = 2;
+            this.has_image.Text = "Add image?";
+            this.has_image.UseVisualStyleBackColor = true;
+            this.has_image.CheckedChanged += new System.EventHandler(this.has_image_CheckedChanged);
+            // 
+            // video_load
+            // 
+            this.video_load.Location = new System.Drawing.Point(23, 29);
+            this.video_load.Name = "video_load";
+            this.video_load.Size = new System.Drawing.Size(190, 23);
+            this.video_load.TabIndex = 3;
+            this.video_load.Text = "Load Video";
+            this.video_load.UseVisualStyleBackColor = true;
+            this.video_load.Click += new System.EventHandler(this.video_load_Click);
+            // 
+            // image_load
+            // 
+            this.image_load.Location = new System.Drawing.Point(23, 66);
+            this.image_load.Name = "image_load";
+            this.image_load.Size = new System.Drawing.Size(190, 23);
+            this.image_load.TabIndex = 4;
+            this.image_load.Text = "Load image";
+            this.image_load.UseVisualStyleBackColor = true;
+            this.image_load.Click += new System.EventHandler(this.image_load_Click);
+            // 
+            // view_mode
+            // 
+            this.view_mode.Controls.Add(this.quad);
+            this.view_mode.Controls.Add(this.dual);
+            this.view_mode.Location = new System.Drawing.Point(374, 66);
+            this.view_mode.Name = "view_mode";
+            this.view_mode.Size = new System.Drawing.Size(207, 125);
+            this.view_mode.TabIndex = 5;
+            this.view_mode.TabStop = false;
+            this.view_mode.Text = "View mode";
+            // 
+            // quad
+            // 
+            this.quad.AutoSize = true;
+            this.quad.Location = new System.Drawing.Point(124, 87);
+            this.quad.Name = "quad";
+            this.quad.Size = new System.Drawing.Size(51, 17);
+            this.quad.TabIndex = 1;
+            this.quad.Text = "Quad";
+            this.quad.UseVisualStyleBackColor = true;
+            this.quad.CheckedChanged += new System.EventHandler(this.quad_CheckedChanged);
+            // 
+            // dual
+            // 
+            this.dual.AutoSize = true;
+            this.dual.Checked = true;
+            this.dual.Location = new System.Drawing.Point(31, 87);
+            this.dual.Name = "dual";
+            this.dual.Size = new System.Drawing.Size(47, 17);
+            this.dual.TabIndex = 0;
+            this.dual.TabStop = true;
+            this.dual.Text = "Dual";
+            this.dual.UseVisualStyleBackColor = true;
+            this.dual.CheckedChanged += new System.EventHandler(this.dual_CheckedChanged);
+            // 
+            // create_video
+            // 
+            this.create_video.Location = new System.Drawing.Point(23, 154);
+            this.create_video.Name = "create_video";
+            this.create_video.Size = new System.Drawing.Size(323, 37);
+            this.create_video.TabIndex = 6;
+            this.create_video.Text = "Create video";
+            this.create_video.UseVisualStyleBackColor = true;
+            this.create_video.Click += new System.EventHandler(this.create_video_Click);
+            // 
+            // name_video
+            // 
+            this.name_video.Location = new System.Drawing.Point(142, 116);
+            this.name_video.Name = "name_video";
+            this.name_video.Size = new System.Drawing.Size(204, 20);
+            this.name_video.TabIndex = 7;
+            this.name_video.TextChanged += new System.EventHandler(this.name_video_TextChanged);
+            // 
+            // label_for_name
+            // 
+            this.label_for_name.AutoSize = true;
+            this.label_for_name.Location = new System.Drawing.Point(23, 119);
+            this.label_for_name.Name = "label_for_name";
+            this.label_for_name.Size = new System.Drawing.Size(99, 13);
+            this.label_for_name.TabIndex = 8;
+            this.label_for_name.Text = "New name of video";
+            // 
+            // four
+            // 
+            this.four.AutoSize = true;
+            this.four.Location = new System.Drawing.Point(374, 33);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(39, 17);
+            this.four.TabIndex = 16;
+            this.four.Text = "V4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.CheckedChanged += new System.EventHandler(this.four_CheckedChanged);
+            // 
+            // third
+            // 
+            this.third.AutoSize = true;
+            this.third.Location = new System.Drawing.Point(329, 33);
+            this.third.Name = "third";
+            this.third.Size = new System.Drawing.Size(39, 17);
+            this.third.TabIndex = 15;
+            this.third.Text = "V3";
+            this.third.UseVisualStyleBackColor = true;
+            this.third.CheckedChanged += new System.EventHandler(this.third_CheckedChanged);
+            // 
+            // second
+            // 
+            this.second.AutoSize = true;
+            this.second.Location = new System.Drawing.Point(284, 33);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(39, 17);
+            this.second.TabIndex = 14;
+            this.second.Text = "V2";
+            this.second.UseVisualStyleBackColor = true;
+            this.second.CheckedChanged += new System.EventHandler(this.second_CheckedChanged);
+            // 
+            // first
+            // 
+            this.first.AutoSize = true;
+            this.first.Location = new System.Drawing.Point(239, 33);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(39, 17);
+            this.first.TabIndex = 13;
+            this.first.Text = "V1";
+            this.first.UseVisualStyleBackColor = true;
+            this.first.CheckedChanged += new System.EventHandler(this.first_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(607, 215);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.third);
+            this.Controls.Add(this.label_for_name);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.name_video);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.create_video);
+            this.Controls.Add(this.view_mode);
+            this.Controls.Add(this.image_load);
+            this.Controls.Add(this.video_load);
+            this.Controls.Add(this.has_image);
+            this.Name = "Form1";
+            this.Text = "VideoEncoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.view_mode.ResumeLayout(false);
+            this.view_mode.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.CheckBox has_image;
+        private System.Windows.Forms.Button video_load;
+        private System.Windows.Forms.Button image_load;
+        private System.Windows.Forms.GroupBox view_mode;
+        private System.Windows.Forms.RadioButton quad;
+        private System.Windows.Forms.RadioButton dual;
+        private System.Windows.Forms.Button create_video;
+        private System.Windows.Forms.TextBox name_video;
+        private System.Windows.Forms.Label label_for_name;
+        private System.Windows.Forms.CheckBox four;
+        private System.Windows.Forms.CheckBox third;
+        private System.Windows.Forms.CheckBox second;
+        private System.Windows.Forms.CheckBox first;
     }
 }
 
--- VideoEncoder/Form1.cs
+++ VideoEncoder/Form1.cs
@@ -1,20 +1,331 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VideoEncoder.Properties;
 
 namespace VideoEncoder
 {
     public partial class Form1 : Form
     {
+        struct Settings
+        {
+            public string video_url { get; set; }
+            public string image_url { get; set; }
+            public string new_name { get; set; }
+            public int materials { get; set; }
+            public bool quad_mode { get; set; }
+            public bool use_image { get; set; }
+
+            public Settings(
+                string videoUrl = null,
+                string imageUrl = null,
+                string newName = "output.mp4",
+                int Materials = 0,
+                bool quadMode = true,
+                bool useImage = false)
+            {
+                video_url = videoUrl;
+                image_url = imageUrl;
+                new_name = newName;
+                materials = Materials;
+                quad_mode = quadMode;
+                use_image = useImage;
+            }
+        }
+
+        Settings Setting = new Settings();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void video_load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Video files|*.mov;*.mp4;*.m4a;*.3gp;*.3g2;*.mj2;";
+            openDialog.FilterIndex = 1;
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openDialog.FileName;
+                Setting.video_url = selectedFile;
+
+                video_load.Text = "Loaded " + System.IO.Path.GetFileName(selectedFile);
+            }
+        }
+
+        private void image_load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files|*.bmp;*.jpg;*.jpeg;*.png";
+            openDialog.FilterIndex = 1;
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openDialog.FileName;
+                Setting.image_url = selectedFile;
+
+                image_load.Text = "Loaded " + System.IO.Path.GetFileName(selectedFile);
+
+                try
+                {
+                    Image image = Image.FromFile(selectedFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+        }
+
+
+        // -------------------------- NAME OF VIDEO --------------------------
+        private void name_video_TextChanged(object sender, EventArgs e)
+        {
+            if (ContainsForbiddenSymbols(name_video.Text))
+            {
+                MessageBox.Show("The name contains forbidden symbols for a file name in Windows.");
+
+                if (name_video.Text.Length > 0)
+                {
+                    name_video.Text = name_video.Text.Substring(0, name_video.Text.Length - 1);
+                    name_video.SelectionStart = name_video.Text.Length;
+                }
+            }
+        }
+        private bool ContainsForbiddenSymbols(string text)
+        {
+            char[] forbiddenSymbols = Path.GetInvalidFileNameChars();
+            return text.Any(c => forbiddenSymbols.Contains(c));
+        }
+        // -------------------------- END --------------------------
+
+
+        // -------------------------- VIEW MODES --------------------------
+        private void dual_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dual.Checked && Setting.materials > 2)
+            {
+                quad.Checked = true;
+                MessageBox.Show("You have a lot of checked video tracks or images. Unchecked it first.");
+            }
+            else if (dual.Checked) Setting.quad_mode = false;
+        }
+
+        private void quad_CheckedChanged(object sender, EventArgs e)
+        {
+            if (quad.Checked) Setting.quad_mode = true;
+        }
+        // -------------------------- END --------------------------
+
+
+        // -------------------------- MATERIALS ADD --------------------------
+        private void first_CheckedChanged(object sender, EventArgs e)
+        {
+            if (first.Checked == false) Setting.materials -= 1;
+            else if (IsALotOf() == true && first.Checked)
+            {
+                first.Checked = false;
+                Setting.materials += 1;
+
+                MessageBox.Show("Your view mode isn't support amount of choosed materials");
+                return;
+            }
+            else if (first.Checked) Setting.materials += 1;
+        }
+
+        private void second_CheckedChanged(object sender, EventArgs e)
+        {
+            if (second.Checked == false) Setting.materials -= 1;
+            else if (IsALotOf() == true && second.Checked)
+            {
+                second.Checked = false;
+                Setting.materials += 1;
+
+                MessageBox.Show("Your view mode isn't support amount of choosed materials");
+                return;
+            }
+            else if (second.Checked) Setting.materials += 1;
+        }
+
+        private void third_CheckedChanged(object sender, EventArgs e)
+        {
+            if (third.Checked == false) Setting.materials -= 1;
+            else if (IsALotOf() == true && third.Checked)
+            {
+                third.Checked = false;
+                Setting.materials += 1;
+
+                MessageBox.Show("Your view mode isn't support amount of choosed materials");
+                return;
+            }
+            else if (third.Checked) Setting.materials += 1;
+        }
+
+        private void four_CheckedChanged(object sender, EventArgs e)
+        {
+            if (four.Checked == false) Setting.materials -= 1;
+            else if (IsALotOf() == true && four.Checked)
+            {
+                four.Checked = false;
+                Setting.materials += 1;
+
+                MessageBox.Show("Your view mode isn't support amount of choosed materials");
+                return;
+            }
+            else if (four.Checked) Setting.materials += 1;
+        }
+
+        private bool IsALotOf()
+        {
+            int limit = Setting.quad_mode ? 4 : 2;
+            return (limit < Setting.materials + 1) ? true : false;
+        }
+
+        // IMAGE
+        private void has_image_CheckedChanged(object sender, EventArgs e)
+        {
+            if (has_image.Checked == false)
+            {
+                Setting.materials--;
+            }
+            else if (IsALotOf() == true && has_image.Checked)
+            {
+                has_image.Checked = false;
+                Setting.materials++;
+
+                MessageBox.Show("Your view mode isn't support amount of choosed materials");
+                return;
+            }
+            else if (has_image.Checked)
+            {
+                Setting.materials++;
+                Setting.use_image = true;
+            }
+        }
+
+        // -------------------------- END --------------------------
+
+
+        // -------------------------- RENDER --------------------------
+        private void create_video_Click(object sender, EventArgs e)
+        {
+            if (Setting.video_url == null)
+            {
+                MessageBox.Show("Video is not loaded");
+                return;
+            }
+            else if (Setting.image_url == null && Setting.use_image)
+            {
+                MessageBox.Show("You check 'Use image', but image is not loaded");
+                return;
+            }
+
+            Setting.new_name = name_video.Text;
+            string directoryPath = Path.GetDirectoryName(Setting.video_url);
+            string new_name = directoryPath + "\\" + Setting.new_name + ".mp4";
+
+            string map = "mosaic";
+            string filter = "";
+
+            if (Setting.materials == 1)
+            {
+                MessageBox.Show("You only have 1 selected material.");
+                return;
+            }
+
+
+            int size = Setting.materials;
+            if (Setting.use_image) size--;
+            int[] tracks = new int[size];
+
+            int entered = 0;
+            if (first.Checked)
+            {
+                tracks[entered] = 0;
+                entered++;
+            }
+            if (second.Checked)
+            {
+                tracks[entered] = 1;
+                entered++;
+            }
+            if (third.Checked)
+            {
+                tracks[entered] = 2;
+                entered++;
+            }
+            if (four.Checked)
+            {
+                tracks[entered] = 3;
+                entered++;
+            }
+
+            if (Setting.materials == 2 && Setting.use_image == false) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[0:v:{tracks[1]}]scale=iw/2:-1[top2];[top1][top2]hstack[mosaic]";
+            else if (Setting.materials == 2 && Setting.use_image) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[1:v]scale=iw/2:-1[top2];[top1][top2]hstack[mosaic]";
+
+            else if (Setting.materials == 3 && Setting.use_image == false) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[0:v:{tracks[1]}]scale=iw/2:-1[top2];[0:v:{tracks[2]}]scale=iw/2:-1[bottom1];[0:v:{tracks[2]}]scale=iw/2:-1[bottom2];[top1][top2]hstack[top];[bottom1][bottom2]hstack[bottom];[top][bottom]vstack[mosaic]";
+            else if (Setting.materials == 3 && Setting.use_image) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[0:v:{tracks[1]}]scale=iw/2:-1[top2];[1:v]scale=iw/2:-1[bottom1];[1:v]scale=iw/2:-1[bottom2];[top1][top2]hstack[top];[bottom1][bottom2]hstack[bottom];[top][bottom]vstack[mosaic]";
+
+            else if (Setting.materials == 4 && Setting.use_image == false) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[0:v:{tracks[1]}]scale=iw/2:-1[top2];[0:v:{tracks[2]}]scale=iw/2:-1[bottom1];[0:v:{tracks[3]}]scale=iw/2:-1[bottom2];[top1][top2]hstack[top];[bottom1][bottom2]hstack[bottom];[top][bottom]vstack[mosaic]";
+            else if (Setting.materials == 4 && Setting.use_image) filter = $"[0:v:{tracks[0]}]scale=iw/2:-1[top1];[0:v:{tracks[1]}]scale=iw/2:-1[top2];[0:v:{tracks[2]}]scale=iw/2:-1[bottom1];[1:v]scale=iw/2:-1[bottom2];[top1][top2]hstack[top];[bottom1][bottom2]hstack[bottom];[top][bottom]vstack[mosaic]";
+
+            string materials = $"-i \"{Setting.video_url}\"";
+            if (Setting.use_image == true) materials += $" -i \"{Setting.image_url}\"";
+
+            //filter = "[0:v:0]scale=iw/2:-1[top1];[0:v:1]scale=iw/2:-1[top2];[top1][top2]hstack[mosaic]";
+            string ffmpegCommand = $"ffmpeg {materials} -filter_complex \"{filter}\" -map \"[{map}]\" -c:v libx264 -c:a ac3 -crf 20 \"{new_name}\"";
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(ffmpegCommand);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+            /*
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {ffmpegCommand}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = false, 
+                UseShellExecute = false
+            };
+
+            using (Process process = new Process { StartInfo = startInfo })
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+            */
+
+            MessageBox.Show("Video is saved in the folder of loaded video!");
+        }
+        
+        // -------------------------- END --------------------------
     }
 }
